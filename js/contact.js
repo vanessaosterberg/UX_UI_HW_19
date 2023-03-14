b3210b25594589ccc85b6f1f3f12562a6c7c64b7
vanessaosterberg <vanessa.osterberg@outlook.com>
2023-03-14 18:05:27 -0500

    Added js

---
+++ js/contact.js
@@ -0,0 +1,6 @@
+var messageFromUser() {
+    var name = document.getElementById(name).value;
+    var email = document.getElementById(email).value;
+    var message = document.getElementById(mess).value;
+    document.write("Thank you for submitting your message!");
+}
